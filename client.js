2ba2e8ad0a447a2c88c79a5648cac7585060f4c5
Kamel Aouidef <kamel.aouidef@logmein.com>
2017-11-28 10:12:23 +0000

    adding the required app files

---
+++ client.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var hostname = '127.0.0.1';
+var port = 3001;
+
+fs.readFile('xi_client.html', function(err, html){
+	if(err){
+		throw err;
+	}
+	var server = http.createServer(function (req, res){
+		res.statusCode = 200;
+		res.setHeader('Content-Type','text/html');
+		res.write(html);
+		res.end();
+	});
+	server.listen(port,hostname,function (){
+		console.log('Server started on port ' +port);
+	});
+});
